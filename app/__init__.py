1378b17b1a57f81b5dc2deb171eebf174c0503b2
achooan <88soldieron@gmail.com>
2019-01-11 15:01:11 +0900

    Edit database directory

--- app/__init__.py
+++ app/__init__.py
@@ -5,18 +5,17 @@ from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 
 from . import commands, db, routes
+from .config import ROOT_DIR
 
 
 def create_app(config=None):
     # Initialize flask app
     app = Flask(__name__)
-    curr_dir = os.path.abspath(os.path.dirname(__file__))
-    base_dir = os.path.join(curr_dir, os.pardir)
 
     # Set database configurations
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
         'DATABASE_URL',
-        'sqlite:///../db.sqlite')
+        f"sqlite:///{os.path.join(ROOT_DIR, 'db.sqlite')}")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     if config is not None:
@@ -30,6 +29,7 @@ def create_app(config=None):
     # Register app to other modules
     db.init_app(app)
     routes.register_blueprint(app)
+    app.url_map.strict_slashes = False
 
     # Add Commands
     app.cli.command()(commands.create_db)
--- app/config.py
+++ app/config.py
@@ -1,3 +1,9 @@
+import os
+
 from dotenv import load_dotenv
 
 load_dotenv()
+
+# Root Directory Config
+_curr_dir = os.path.abspath(os.path.dirname(__file__))
+ROOT_DIR = os.path.join(_curr_dir, os.pardir)
