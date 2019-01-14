277a76e73c8f8888bae188eac17e22463cdf14ae
achooan <88soldieron@gmail.com>
2019-01-14 11:44:02 +0900

    Fix Heroku deploy issue

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from app import create_app
+import os
+
+from app import config, create_app
 
 if __name__ == '__main__':
-    create_app().run(debug=True)
+    create_app().run(debug=True,
+                     host='0.0.0.0',
+                     port=int(os.environ.get('PORT', 5000)))
